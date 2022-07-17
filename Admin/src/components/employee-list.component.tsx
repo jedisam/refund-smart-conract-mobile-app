f48d08186492b451683353c6cbce44b57d7f504d
Yididiya <yidisam18@gmail.com>
2022-07-17 07:40:17 +0300

    show all employees func

--- Admin/src/components/employee-list.component.tsx
+++ Admin/src/components/employee-list.component.tsx
@@ -1,70 +1,143 @@
-import { useEffect } from 'react';
+import { useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { getEmployees, getEmployeesLoading } from '../selectors/selectors';
 import { loadEmployeesInProgress, removeEmployee, loadEmployeeInProgress } from '../actions/employee'
-import { IEmployee, IId } from '../typeDefs'
+import { IId } from '../typeDefs'
+import { ethers } from 'ethers';
 
-const Employee = (props: any) => {
-  return (
-    <tr>
-      <td>{props.employee.name}</td>
-      <td>{props.employee.address}</td>
-      <td>{props.employee.latitude}</td>
-      <td>{props.employee.longtitude}</td>
-      <td>{props.employee.timestamp}</td>
-      <td>
-        <Link to={'/edit/' + props.employee._id} onClick={() => {props.loadEmployee(props.employee._id)}}>edit</Link> |{' '}
-        <button
-        style={{border: 'none', color: 'red'}}
-          onClick={() => {
-            props.deleteEmployee(props.employee._id);
-          }}
-        >
-          delete
-        </button>
-      </td>
-    </tr>
-  );
-};
+import { abi, contractAddress } from '../constants/endPoints';
+import { Button } from 'reactstrap';
+
+
 
 const EmployeeList = (props: any) => {
-  const {startLoadingEmployees} = props 
-  useEffect(() => {
-    startLoadingEmployees();
-  }, [startLoadingEmployees]);
+  // const [isConnected, setIsConnected] = useState(false);
+  const [emps, setEmps] = useState([]);
+  const [isShown, setIsShown] = useState(false);
+  // const {startLoadingEmployees} = props 
+  // useEffect(() => {
+  //   // alert('State Changed')
+  //   employees()
+  // }, []);
+
+  const checkStatus = async (address: any) => {
+      if (window.ethereum) {
+        try {
+           // res[0] for fetching a first wallet
+        window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        .then((res: any) => {
+          console.log(res[0])
+          // setIsConnected(true);
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          const signer = provider.getSigner();
+          console.log(signer)
+          const contract = new ethers.Contract(contractAddress, abi, signer);
+          console.log(contract)
+          const transactionRsponse =  contract.employeeCondition(address);
+          transactionRsponse.then((response: any) => {
+            console.log(response)
+            if (response) {
+              alert("Employee is in compliance with the agreement")
+            } else{
+            alert("Employee is out-of compliance")
+            }
+          // //   // startLoadingEmployee s(response)
+          })
+        });
+        } catch (error) {
+          console.log(error)
+        }
+       
+      } else {
+        alert("install metamask extension!!");
+      }
+    
+  }
+  const Employee = (props: any) => {
+    return (
+      <tr>
+        <td>{props.employee.name}</td>
+        <td>{props.employee.address}</td>
+        <td>{props.employee.latitude}</td>
+        <td>{props.employee.longtitude}</td>
+        <td>{props.employee.distance}</td>
+        <td>
+         <Button onClick={() => checkStatus(props.employee.address)} color="primary">Check Status</Button>  
+        </td>
+      </tr>
+    );
+  };
   const employees = () => {
-    const loadingMessage = <div>Loading Employees...</div>;
-    const content = props.employeesR.map((employee: IEmployee) => {
-      // console.log('The Emp: ', employee);
+    
+    console.log("Okay from employees function: ", emps)
+    // const loadingMessage = <div>Loading Employees...</div>;
+   
+    return emps.map((employee: any) => {
+      console.log('The Empppp: ', employee);
       return (
-        <Employee
+        <Employee   
           employee={employee}
-          loadEmployee={props.loadEmployee}
-          deleteEmployee={props.onRemovePressed}
-          key={employee?._id}
+          key={employee.address}
         />
       );
     });
-    return props.isLoading ? loadingMessage : content;
+    // return props.isLoading ? loadingMessage : content;
   };
+
+  async function connect() {
+    if (window.ethereum) {
+      // try {
+         // res[0] for fetching a first wallet
+      try {
+        let arr: any = [] 
+        await
+        window.ethereum
+        .request({ method: "eth_requestAccounts" })
+        // setIsConnected(true);
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(contractAddress, abi, signer);
+        console.log(contract)
+        const getEmployees =  await contract.getEmployees();
+        console.log(getEmployees.length)
+        for (let i = 0; i < getEmployees.length; i++) {
+          const empDetail =  await contract.getEmployeeDetail(getEmployees[i]);
+          await arr.push({address: getEmployees[i], name: empDetail[0].toString(), latitude: empDetail[1].toString(), longtitude:empDetail[2].toString(), distance: empDetail[3].toString()})
+          
+        }
+        setEmps(arr)
+        setIsShown(true)      
+       
+      } catch (error) {
+        console.log(error)
+      }
+     
+    } else {
+      alert("install metamask extension!!");
+    }
+  }
 
   return (
     <div>
       <h3>Employees</h3>
-      <table className="table">
+      <Button onClick={connect} color="info">Show Employees</Button>
+      {isShown && (
+        <table className="table">
         <thead className="thead-light">
           <tr>
             <th>Name</th>
             <th>Address</th>
             <th>Latitude Range</th>
             <th>Longtude Range</th>
-            <th>TimeStamp</th>
+            <th>Distance</th>
             <th>Condition</th>
           </tr>
         </thead>
-        <tbody>{employees()}</tbody>
+        <tbody>{emps.length === 0 ? undefined : employees()}</tbody>
       </table>
+      )}
+      
     </div>
   );
 };
